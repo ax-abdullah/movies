77acda6a6907cdfb64e0896f73f431649be139fd
Abdullah Ahmed <abdullah.work06@gmail.com>
2022-06-21 11:06:39 +0200

    adding the register form and generating authentication service

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -8,14 +8,16 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class RegisterComponent implements OnInit {
   registerForm:FormGroup = new FormGroup({
-    first_name: new FormControl(null, [Validators.maxLength(8), Validators.minLength(3)]),
-    last_name: new FormControl(null),
-    email: new FormControl(null),
-    password: new FormControl(null),
-    age: new FormControl(null)
+    first_name: new FormControl(null, [ Validators.minLength(3), Validators.required]),
+    last_name: new FormControl(null, [Validators.minLength(3), Validators.required]),
+    email: new FormControl(null, [Validators.email, Validators.required]),
+    password: new FormControl(null, [Validators.required, Validators.pattern(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/)]),
+    age: new FormControl(null, [Validators.required, Validators.min(16)])
   });
   submitRegister(form:FormGroup):void{
     console.log(form);
+    console.log(form.value)
+
   }
   constructor() { }
 
