2e00f4228ca5c5a348abe9c9fe2edf8b2c1c7c83
abdullah <abdullah.work06@gmail.com>
2022-06-22 22:11:29 +0200

    log user out after a given time

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { AuthService } from './auth.service';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'movies-app';
+
+  constructor(private _AuthService: AuthService) {
+    // log user out if after a specific amout of time
+    // _AuthService.userData.subscribe(() =>{
+    //   if(_AuthService.userData.getValue() != null) setTimeout(() => {
+    //     _AuthService.logout()
+    //   }, 5000);
+    // })
+  }
+
 }
