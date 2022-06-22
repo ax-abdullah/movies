906d81c980a8e925941203e75c72a53d79bc7ba8
abdullah <abdullah.work06@gmail.com>
2022-06-22 21:42:12 +0200

    handling logout

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,14 +1,15 @@
+import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import jwtDecode from 'jwt-decode';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   BASE_URL:string= 'https://route-egypt-api.herokuapp.com/';
-  userData:object = {};
-  constructor(private _HttpClient: HttpClient) { }
+  userData= new BehaviorSubject(null);
+  constructor(private _HttpClient: HttpClient, private _Router: Router) { }
   register(data: object):Observable<any>{
     return this._HttpClient.post(`${this.BASE_URL}signup`, data);
   }
@@ -17,8 +18,14 @@ export class AuthService {
   }
   setUserData():void{
     let encodedToken:string = JSON.stringify(localStorage.getItem('userData'));
-    let decodedToken:object = jwtDecode(encodedToken);
-    console.log(decodedToken); 
+    let decodedToken:any = jwtDecode(encodedToken);
+    console.log(decodedToken);
+    this.userData.next(decodedToken)
+  }
 
+  logout():void{
+    localStorage.removeItem('userData');
+    this.userData.next(null);
+    this._Router.navigate(['/login'])
   }
 }
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
-
-  constructor() { }
+  isLoggedIn:boolean = false
+  constructor(private _AuthService: AuthService, private _Router: Router) { }
 
   ngOnInit(): void {
-  }
 
+      this._AuthService.userData.subscribe(() =>{
+        if(this._AuthService.userData.getValue() != null){
+          this.isLoggedIn = true;
+
+        }
+        else {
+          this.isLoggedIn = false
+          // maybe will remove this shit
+          this._Router.navigate(['/login'])
+        };
+        console.log(this.isLoggedIn);
+      })
+
+  }
+  logout():void{
+    this._AuthService.logout()
+  }
 }
