1f55350c6be4e96e116b6d875e97f993b47300c2
abdullah <abdullah.work06@gmail.com>
2022-06-25 18:40:53 +0200

    adding home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,3 +1,4 @@
+import { MoviesService } from './../movies.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  trendingMovies:any[] = [];
+  trendingTv:any[]= [];
+  trendingPeople: any[] = [];
+  imgPrefix:string = 'https://image.tmdb.org/t/p/w500';
+  constructor(private _MoviesService: MoviesService) { }
 
   ngOnInit(): void {
+    this._MoviesService.getTrending('movie').subscribe((response)=>{
+      this.trendingMovies = response?.results.slice(0, 10);
+      console.log(this.trendingMovies);
+    });
+    this._MoviesService.getTrending('tv').subscribe(response => {
+      this.trendingTv = response?.results.slice(0,10);
+      console.log(this.trendingTv);
+    });
+    this._MoviesService.getTrending('person').subscribe(response => {
+      this.trendingPeople = response?.results.filter((el:any) => el.profile_path).slice(0,10)
+      console.log(this.trendingPeople);
+    })
   }
 
 }
--- src/app/movies.service.ts
+++ src/app/movies.service.ts
@@ -6,8 +6,13 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class MoviesService {
+  // an example API request: https://api.themoviedb.org/3/movie/550?api_key=118ef4e4bc016839242aa18fc1008c7a
 
-  constructor(private _HttpClient: HttpClient) {
-    return
+  KEY:string = '118ef4e4bc016839242aa18fc1008c7a';
+  URL:string = 'https://api.themoviedb.org/3/';
+  constructor(private _HttpClient: HttpClient) {}
+
+  getTrending(mediaType:string):Observable<any> {
+    return this._HttpClient.get(`${this.URL}/trending/${mediaType}/week?api_key=${this.KEY}`)
   }
 }
